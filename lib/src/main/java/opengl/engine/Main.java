ea0c86b595d28c54c8f757e96e3a56881f472fdd
toxicvipa <robin.tretter@gmail.com>
2022-02-04 10:37:14 +0100

    created window

---
+++ lib/src/main/java/opengl/engine/Main.java
@@ -0,0 +1,12 @@
+package opengl.engine;
+
+import opengl.engine.display.Window;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Window window = Window.getInstance();
+		window.run();
+	}
+	
+}
---
+++ lib/src/main/java/opengl/engine/display/Window.java
@@ -0,0 +1,80 @@
+package opengl.engine.display;
+
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.system.MemoryUtil;
+
+public class Window {
+
+	private int width, height;
+	private String title;
+	private long glfwWindow;
+	
+	private static Window INSTANCE;
+	
+	private Window() {
+		width = 1920;
+		height = 1200;
+		title = "Engine";
+	}
+	
+	public void run() {
+		System.out.println("LWJGL Version: " + Version.getVersion() + "!");
+		init();
+		loop();
+	}
+	
+	private void init() {
+		// Error callback
+		GLFWErrorCallback.createPrint(System.err).set();
+		
+		// Initialize GLFW
+		if(!GLFW.glfwInit()) {
+			throw new IllegalStateException("Unable to initialize GLFW");
+		}
+		
+		// Configure GLFW
+		GLFW.glfwDefaultWindowHints();
+		GLFW.glfwWindowHint(GLFW.GLFW_VISIBLE, GLFW.GLFW_FALSE);
+		GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GLFW.GLFW_TRUE);
+		GLFW.glfwWindowHint(GLFW.GLFW_MAXIMIZED, GLFW.GLFW_TRUE);
+		
+		// Create window
+		glfwWindow = GLFW.glfwCreateWindow(width, height, title, MemoryUtil.NULL, MemoryUtil.NULL);
+		if(glfwWindow == MemoryUtil.NULL) {
+			throw new IllegalStateException("Failed to create window");
+		}
+		
+		// Make the context current
+		GLFW.glfwMakeContextCurrent(glfwWindow);
+		// Enable v-sync
+		GLFW.glfwSwapInterval(1);
+		// Show window
+		GLFW.glfwShowWindow(glfwWindow);
+		// Important
+		GL.createCapabilities();
+	}
+	
+	private void loop() {
+		while(!GLFW.glfwWindowShouldClose(glfwWindow)) {
+			// Poll events
+			GLFW.glfwPollEvents();
+			
+			GL11.glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+			
+			GLFW.glfwSwapBuffers(glfwWindow);
+		}
+	}
+	
+	public static Window getInstance() {
+		if(INSTANCE == null) {
+			INSTANCE = new Window();
+		}
+		return INSTANCE;
+	}
+	
+}
